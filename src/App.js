bea3b53bc4f7587c975151ad15a1298c4c314af0
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-04-16 12:05:30 +0330

    route specificity

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Home } from './pages/Home';
 import { Contact } from './pages/Contact';
 import { Book } from './pages/Book'
 import { BookList } from './pages/BookList';
+import { NewBook } from './pages/NewBook';
+import { NotFound } from './pages/NotFound';
 
 
 function App() {
@@ -12,21 +14,18 @@ function App() {
     <>
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/books">Book</Link>
-          </li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/books">Book</Link></li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/books/:id" element={<BookList />} />
+        <Route path="/books/" element={<BookList />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/books/new" element={<NewBook />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -5,6 +5,10 @@ export function BookList() {
         <>
             <h1>BookList</h1>
             <Link to="/books/1">Book 1</Link>
+            <br />
+            <Link to="/books/2">Book 2</Link>
+            <br />
+            <Link to="/books/new">New Book</Link>
         </>
     )
 }
